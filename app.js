7ef091cafe4c7fef3a8fdc4d8af4a1e7b8be3c64
mmillefly <c.mello@flyeducacao.org>
2024-12-17 20:28:49 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,8 @@
+let titulo = document.querySelector('h1');
+titulo.innerHTML = 'Jogo do Numero Secreto';
+let pa = document.querySelector('p');
+pa.innerHTML = 'Escolha um número'
+
+function verificarChute(){
+    console.log('click')
+}
